47403179a473487fa286e1d4aa5766f6c08e477b
dgistando <davidgistando@gmail.com>
2018-04-11 11:52:49 -0700

    Added a sample request to zomato for tacobell list

--- components/restaurant_search.js
+++ components/restaurant_search.js
@@ -34,3 +34,46 @@ export default class Restaurant extends Component{
         );
     }
 }
+
+/**
+ import React from "react";
+ import { render } from "react-dom";
+ import { makeData, Logo, Tips } from "./Utils";
+
+ // Import React Table
+ import ReactTable from "react-table";
+ import "react-table/react-table.css";
+
+ class App extends React.Component {
+  constructor() {
+    super();
+
+    const Zomato = require('zomato.js');
+    const z = new Zomato('89272d1f40c917674515b3364c6f2f96');
+
+    this.state = {
+      data: makeData()
+    };
+
+    z.categories()
+     .then(function (data) {
+        console.log(data);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+
+  render() {
+    const { data } = this.state;
+
+
+
+    return (
+      <div>Test</div>
+    );
+  }
+}
+
+ render(<App />, document.getElementById("root"));
+ */
